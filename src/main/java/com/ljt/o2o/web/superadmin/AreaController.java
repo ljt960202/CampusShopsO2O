b9a080f14470587c01975bf213a9a4cad3860453
Duplicator <ljt199622ljt@163.com>
2019-05-29 14:21:26 +0800

    logback配置和使用

--- src/main/java/com/ljt/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/ljt/o2o/web/superadmin/AreaController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.ljt.o2o.entity.Area;
 import com.ljt.o2o.service.AreaService;
 
+
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+	Logger logger = LoggerFactory.getLogger(AreaController.class);	
 	@Autowired	
 	private AreaService areaService;
 	
 	@RequestMapping(value="/arealist",method = RequestMethod.GET)
 	@ResponseBody
 	public Map<String,Object> areaList(){
+		logger.info("===start===");
+		long startTime = System.currentTimeMillis();
 		Map<String,Object> modelMap = new HashMap<String,Object>();
 		List<Area> areaList = new ArrayList<Area>();
 		try {
@@ -34,6 +40,10 @@ public class AreaController {
 			modelMap.put("success",false);
 			modelMap.put("errMsg", e.toString());
 		}
+		logger.error("test error!");
+		long endTime = System.currentTimeMillis();
+		logger.debug("costTime:[{}ms]",endTime-startTime);
+		logger.info("===end===");
 		return modelMap;
 	}
 
